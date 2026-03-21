ee85c83fd360accd041d95b3a05a68e2fc382adf
Ankit Pandey <36405347+Ankitgkp@users.noreply.github.com>
2026-03-22 03:56:56 +0530

    Remove t

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [countt, setCountt] = useState(0)
+  const [count, setCount] = useState(1)
 
   return (
     <>
